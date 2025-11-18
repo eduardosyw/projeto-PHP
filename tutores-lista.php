722c2fec4208b52c988a608532c7d311f3591426
eduardosyw <eduardosyw@gmail.com>
2025-11-18 20:16:09 -0300

    Implement confirmation for tutor deletion
    Add confirmation prompt for tutor deletion

--- tutores-lista.php
+++ tutores-lista.php
@@ -18,7 +18,11 @@ echo "<a href='tutores-cadastro-form.php'> Novo Tutor </a>";
         
         <a href="tutores-editar-form.php?id=<?= $tutor['id'] ?>">Editar</a>
         <a href="tutores-excluir.php?id=<?= $tutor['id'] ?>" style="color: red;">Excluir</a>
+        <a href="tutores-excluir.php?id=<?=$tutor['id'] ?>"
+            onclick="return confirm('Tem certeza que deseja excluir esse cadastro?');"
+            style="color: red;"></a>
     </li>
     <?php endforeach; ?>
 
 </ul>
+
