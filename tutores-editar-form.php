85d5becbdb2c6a6e0b684b9434d7fa409cd7300f
eduardosyw <eduardosyw@gmail.com>
2025-11-18 20:37:14 -0300

    arquivo para editar tutor/client

---
+++ tutores-editar-form.php
@@ -0,0 +1,28 @@
+<?php
+
+$id = $_GET['id'];
+
+$tutores = json_decode(file_get_contents("tutores.json"), true);
+
+$tutor_atual = null;
+foreach ($tutores as $t) {
+    if ($t['id'] == $id) {
+        $tutor_atual = $t;
+        break;
+    }
+}
+?>
+
+<h2>Editar Tutor</h2>
+
+<form method="post" action="tutores-editar.php">
+    <label>Nome</label>
+    <input type="text" name="nome"><br>
+    <label>Cpf</label>
+    <input type="text" name="cpf"><br>
+    <label>Telefone</label>
+    <input type="text" name="telefone"><br>
+    <label>Endereço Completo</label>
+    <input type="text" name="endereco"><br>
+    <input type="submit" value="Salvar Alterações">
+    </form>
